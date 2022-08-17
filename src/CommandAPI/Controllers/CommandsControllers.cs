a40c325bead2bbc7b986b7978d0ae343569743fa
Alfredo Sotero <alfredosotero@hotmail.com>
2022-08-17 09:41:24 -0500

    Added Command Model to API Project

--- src/CommandAPI/Controllers/CommandsControllers.cs
+++ src/CommandAPI/Controllers/CommandsControllers.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.JsonPatch;
 
 namespace CommandAPI.Controllers
 {
-
+    /*controllers de api*/
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase
---
+++ src/CommandAPI/Models/Otr.cs
@@ -0,0 +1,8 @@
+namespace CommandAPI.Models
+{
+    public class Otr
+    {
+        public int Id { get; set; }
+        public string Comments { get; set; }
+    }
+}
